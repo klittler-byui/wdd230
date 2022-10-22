27a0d9afabbac80da7ec6d4f7696252f49eaef14
klittler-byui <pea21002@byui.edu>
2022-10-22 09:54:44 -0700

    Sat AM wind chill pulling from DOM

--- 04-Chamber/04js/windchill.js
+++ 04-Chamber/04js/windchill.js
@@ -1,12 +1,17 @@
 
-
-          let t = 45;
-          let s = 15;
-          let result = windChill(t, s);
-          document.getElementById("output").innerHTML = "Wind Chill: " + result  + "<sup>o</sup>F";
+let input1 = document.querySelector('#input1');
+let input2 = document.querySelector('#input2');
 
 
-      function windChill(temp, speed){
+// PROCESSING
+let t = parseFloat(input1.textContent);
+let s = parseFloat(input2.textContent);
+
+let result = windChill(t, s);
+
+document.getElementById("output").innerHTML = "Wind Chill: " + result  + "<sup>o</sup>F";
+
+function windChill(temp, speed){
           // Use provided formula to calculate wind chill.
           //  f = 35.74 + 0.6215t - 35.75s^0.16 + 0.4275t s^0.16
           let farenheit = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
