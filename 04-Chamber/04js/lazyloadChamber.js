960c57a38b96b8bb508e5a6f2f8059505fa3fad3
klittler-byui <pea21002@byui.edu>
2022-10-26 20:45:08 -0700

    w07 chamber lazy loading set

---
+++ 04-Chamber/04js/lazyloadChamber.js
@@ -0,0 +1,41 @@
+// Resources:
+// MDN - https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Loading
+// Bro. Blazzard video - https://video.byui.edu/media/t/0_0tixbudg
+
+
+
+
+// get all images with data-src attribute
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+
+// optional parameters being set for the Intersectional Observer
+const imgOptions = {
+  threshold: 1,
+  rootMargin: "0px 0px 50px 0px"
+};
+
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {image.removeAttribute('data-src');};
+};
+
+// First check to see if Interection Observer is supported
+if('IntersectionObserver' in window) {
+  const imgObserver = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  }, imgOptions);
+
+  imagesToLoad.forEach((img) => {
+    imgObserver.observe(img);
+  });
+}
+else {
+  loadImages.forEach((img) => {
+    loadImages(img);
+  });
+}
