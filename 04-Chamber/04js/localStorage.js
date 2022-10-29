7d6abd02b5893122e8b704af91fa1f912b7b6dfa
klittler-byui <pea21002@byui.edu>
2022-10-28 19:53:17 -0700

    W07 all but the days since

--- 04-Chamber/04js/localStorage.js
+++ 04-Chamber/04js/localStorage.js
@@ -1,18 +1,17 @@
-// Calculate the number of days between two dates
+const visitsDisplay = document.querySelector(".visits");
+const lastVisitDisplay = document.querySelector(".lastVisit");
 
-// To set two dates two variables
-let lastDate1 = new Date ("06/19/2022");
-let todayDate = new Date ("07/30/2022");
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
 
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
 
-/// To calculate the time difference of two dates
-let Difference_In_Time = todayDate.getTime () - lastDate1.getTime();
+} else {
+	visitsDisplay.textContent = `This is your first visit!`;
+}
 
-// To calculate the no of days between two dates
-let Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
+numVisits++;
+localStorage.setItem("visits-ls", numVisits);
+todayDate.textContent = Date.now();
 
-// To calculate the final no. of days (result)
-document.write("Total number of days since your last visit <br>"
-  + lastDate1 + "<br> and <br> "
-  + todayDate + " is: <br> "
-  + Difference_In_Days);
+let lastVisit = Number(window.localStorage.getItem("lastVisit"));
