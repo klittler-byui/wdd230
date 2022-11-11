251f34b38ae6e06f04f25e461e938bf163c371a5
klittler-byui <pea21002@byui.edu>
2022-11-10 19:21:26 -0700

    W09 activity adjustment

--- 09-Lesson/prophets2.js
+++ 09-Lesson/prophets2.js
@@ -32,9 +32,10 @@ function displayProphets (prophet){
 
     // Add/append the section(card) with the h2 element
     card.appendChild(h2);
+    card.appendChild(portrait);
     card.appendChild(birthdate);
     card.appendChild(birthplace);
-    card.appendChild(portrait);
+
 
     // Add/append the existing HTML div with the cards class with the section(card)
     document.querySelector('div.cards').appendChild(card);
