7e7fcecaa0299f6ac349989b0d2d8f1b4f0d505f
klittler-byui <pea21002@byui.edu>
2022-11-11 15:45:33 -0700

    w09 json working

--- 04-Chamber/04js/09directory.js
+++ 04-Chamber/04js/09directory.js
@@ -1,6 +1,6 @@
 const requestURL = 'https://raw.githubusercontent.com/klittler-byui/wdd230/main/04-Chamber/04js/directorylist.json';
 
-const cards = document.querySelector('.cards');
+const Mcards = document.querySelector('.Mcards');
 
 fetch(requestURL)
 .then(function (response) {
@@ -13,26 +13,26 @@ fetch(requestURL)
 });
 
 function displayMembers (member) {
-  let DIRcard = document.createElement('section');
+  let Mcard = document.createElement('section');
   let h2 = document.createElement('h2');
   let logo = document.createElement('img');
   let address = document.createElement('span');
   let phone = document.createElement('span');
-  let web - document.createElement('span');
+  let web = document.createElement('span');
 
-  h2.textContent = `${member.bname}`;
-  address.textContent = `${member.address} ${member.city}, ${member.state} ${member.zip}`;
+  h2.textContent = `${member.name}`;
+  address.textContent = `${member.address}`;
   phone.textContent = `${member.phone}`;
   web.textContent = `${member.web}`;
 
   logo.setAttribute('src', member.logoImg);
-  logo.setAttribute('alt', `${member.bname} logo`);
+  logo.setAttribute('alt', `${member.name} logo`);
   logo.setAttribute('loading', 'lazy');
 
-  DIRcard.appendChild(logo);
-  DIRcard.appendChild(address);
-  DIRcard.appendChild(phone);
-  DIRcard.appendChild(web);
+  Mcard.appendChild(logo);
+  Mcard.appendChild(address);
+  Mcard.appendChild(phone);
+  Mcard.appendChild(web);
 
-  document.querySelector('div.cards').appendChild(card);
+  document.querySelector('div.Mcards').appendChild(Mcard);
 }
