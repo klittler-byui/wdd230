d7cad91f5a6a3a0a85468c298d49bb1e53c56904
klittler-byui <pea21002@byui.edu>
2022-11-11 12:44:24 -0700

    w09 Json file

---
+++ 04-Chamber/04js/09chamber.js
@@ -0,0 +1,84 @@
+/************* LAST MODIFIED ***************/
+document.querySelector(
+	"#lastModified"
+).textContent = `Last Modification: ${document.lastModified}`;
+
+/**** NUMERIC DATE xx/xx/xxxx ********/
+const date1 = document.querySelector("#date1");
+
+try {
+	const options = {
+		day: "numeric",
+		month: "numeric",
+		year: "numeric",
+	};
+	date1.innerHTML = `${new Date().toLocaleDateString("en-US", options)}</span>`;
+} catch (e) {}
+
+
+/**** NUMERIC DATE AND TIMExx/xx/xxxx 00:00 ********/
+const dateTime = document.querySelector("#dateTime");
+
+try {
+	const options = {
+		day: "numeric",
+		month: "numeric",
+		year: "numeric",
+		hour: "numeric",
+		minutes: "numeric",
+	};
+	dateTime.innerHTML = `${new Date().toLocaleDateString("en-US", options)}</span>`;
+} catch (e) {}
+
+/**** ALPHA-NUMERIC DATE Month Day, Year ********/
+const date2 = document.querySelector("#date2");
+
+try {
+	const options = {
+		day: "numeric",
+		month: "long",
+		year: "numeric"
+	};
+	date2.innerHTML = `${new Date().toLocaleDateString("en-US", options)}</span>`;
+} catch (e) {}
+
+
+const copyYr = document.querySelector("#copyYr");
+// Try to complete the method with options
+try {
+	const options = {
+		year: "numeric"
+	};
+	copyYr.innerHTML = `${new Date().toLocaleDateString("en-US", options)}</span>`;
+} catch (e) {}
+
+
+
+// Menu Hamburger Button
+function toggleMenu() {
+  document.getElementById("primaryNav").classList.toggle("open");
+  document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.getElementById('hamburgerBtn');
+x.onclick = toggleMenu;
+
+
+
+
+// TUES WED HERO BANNER //
+
+
+let bannerDay = new Date().getDay();
+const announcement = document.querySelector('#announcement');
+if (bannerDay === 1 || bannerDay === 2){
+announcement.style.display = 'block';
+} else{
+announcement.style.display = 'none';
+}
+const close = document.querySelector('#close');
+close.addEventListener('click', () => {
+banner.style.display = 'none';
+})
+
+/**************** DIRECTORY CARDS *******************/
