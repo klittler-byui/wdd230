02af353d09a81be72a22535ae01ccb60ecd4b103
klittler-byui <pea21002@byui.edu>
2022-11-23 13:19:40 -0700

    w11 incomplete Spot api

--- 04-Chamber/04js/09directory.js
+++ 04-Chamber/04js/09directory.js
@@ -52,7 +52,7 @@ const gridbutton = document.querySelector("#grid");
 const listbutton = document.querySelector("#list");
 const display = document.querySelector(".Mcards");
 
-// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
+
 
 gridbutton.addEventListener("click", () => {
 	// example using arrow function
