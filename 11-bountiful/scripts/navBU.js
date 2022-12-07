5d36a48947d0eeb3e6df100377ccd20f1833972f
klittler-byui <pea21002@byui.edu>
2022-12-06 18:05:28 -0700

    w13 start

---
+++ 11-bountiful/scripts/navBU.js
@@ -0,0 +1,8 @@
+// Menu Hamburger Button
+function toggleMenu() {
+  document.getElementById("primaryNav").classList.toggle("open");
+  document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.getElementById('hamburgerBtn');
+x.onclick = toggleMenu;
